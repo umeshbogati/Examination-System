799180c80dece19f4215a2ba878a1d909aa0c7bc
umeshbogati <bogatiu17@gmail.com>
2025-12-24 22:32:02 +0545

    here i make urls.py for exam and connect url.py with examsystemurl.py . Also make html templates for login ,take exam and exam list.

---
+++ examsystem/exam/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.student_login, name='login'),
+    path('logout/', views.student_logout, name='logout'),
+    path('exams/', views.exam_list, name='exam_list'),
+    path('exam/<int:exam_id>/', views.take_exam, name='take_exam'),
+]
