84def7a433e655e790287b4b477263176d7a2b7d
umeshbogati <bogatiu17@gmail.com>
2025-12-25 21:49:22 +0545

    I have change views.py where I create account where student can login and give exam

--- examsystem/exam/views.py
+++ examsystem/exam/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from .models import Exam, Question
-
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 # Create your views here.
 def student_login(request):
@@ -45,3 +46,19 @@ def take_exam(request, exam_id):
             
         })
     return render(request, 'take_exam.html', {'questions': questions})
+
+def student_register(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+
+        
+        if User.objects.filter(username=username).exists():
+            messages.error(request, "Username already exists")
+        else:
+            User.objects.create_user(username=username, password=password)
+            messages.success(request,"Account created successfully")
+            return redirect('login')
+        return render(request, 'register.html')
+        
+        
